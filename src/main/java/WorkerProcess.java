4515cb557c561b22dc48b79b1cf39831208e28a1
Vasco Pedro <vasco.pedro@cgi.com>
2021-07-06 18:23:05 +0100

    Refactor architecture update

--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -22,8 +22,8 @@ public class WorkerProcess {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
         String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+        String password = dbUri.getUserInfo().split(":")[1];        
+        String dbUrl = "jdbc:postgres://djyfywgcyljccc:1aefd385de78d48a6962515308d9f7b3320a9e996e54082dc32510e141d59ca9@ec2-54-217-195-234.eu-west-1.compute.amazonaws.com:5432/ddc19nf5cgufb";
 
         return DriverManager.getConnection(dbUrl, username, password);
     }
