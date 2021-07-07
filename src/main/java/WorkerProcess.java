b5b064b3cfb0118449561defa5cf0f3b3ec38987
Vasco Pedro <vasco.pedro@cgi.com>
2021-07-07 16:25:23 +0100

    Refactor architecture update

--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -7,15 +7,23 @@ public class WorkerProcess {
     public static void main(String[] args) throws Exception {
         
         Connection connection = getConnection();
+        ResultSet rs = setupTable(connection);
         
-        Statement stmt = connection.createStatement();
+         while (rs.next()) {
+            
+                System.out.print(rs.getInt(1));
+                System.out.print(": ");
+                System.out.println(rs.getString(2));
+            }
+        
+        /*Statement stmt = connection.createStatement();
         stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
         stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
         stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
         ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
         while (rs.next()) {
             System.out.println("Read from DB: " + rs.getTimestamp("tick"));
-        }
+        }*/
     }
     
     private static Connection getConnection() throws URISyntaxException, SQLException {
@@ -27,6 +35,12 @@ public class WorkerProcess {
 
         return DriverManager.getConnection(dbUrl, username, password);
     }
-   
-
+    
+    private static ResultSet setupTable (Connection con) throws SQLException {
+         
+        PreparedStatement pst = con.prepareStatement("SELECT * FROM ticks");
+        ResultSet rs = pst.executeQuery();
+        return rs;
+    }
 }
+  
